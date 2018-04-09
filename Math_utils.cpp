8d46febbee8b7f4d72dd7977168ad4a48858fe8f
Robert Ladd <ladd.rj@gmail.com>
2018-04-08 21:28:31 -0600

    equal parity is done

--- Math_utils.cpp
+++ Math_utils.cpp
@@ -21,9 +21,27 @@ int Math_utils::GetDigit(int num, int place){
 
 //— returns true iff x and y have equal parity (both odd or both even)
 bool Math_utils::EqualParity(int x, int y){
-  return false;
+  int x1 = x % 2;
+  int y1 = y % 2;
+  
+  if (x1 == y1){
+      return true;
+  }
+
+  else{
+      return false;
+  }
+
+
 }
 // — returns true iff all numbers in nums have equal parity (both odd or both even)
 bool Math_utils::EqualParity(std::vector<int> nums){
-  return false;
+  int check = nums[0] % 2;
+  for (int i=1; i < nums.size(); i++){
+         if ((nums[i] % 2) != check ){
+             return false;
+         }
+  }
+
+  return true;
 }
--- test.cpp
+++ test.cpp
@@ -13,3 +13,20 @@ TEST_CASE ( "Get dight at nth place", "[GetDigit]") {
   REQUIRE( Math_utils::GetDigit(12347, 1000)==2);
   CHECK_THROWS( Math_utils::GetDigit(17,100));
 }
+TEST_CASE ( "Test whether two integers have equal parity", "[EqualParity]") {
+  REQUIRE( Math_utils::EqualParity(3, 1) ==true);
+  REQUIRE( Math_utils::EqualParity(12, 2) == true);
+  REQUIRE( Math_utils::EqualParity(1, 2)==false);
+  
+}
+TEST_CASE ( "Test whether all numbers in the vector have equal parity", "[EqualParity]") {
+  std::vector<int> nums1 {0,2,4,6,8,10};
+  std::vector<int> nums2 {1,3,5,7,9,11};
+  std::vector<int> nums3 {0,2,4,5,8,10};
+  
+  
+  REQUIRE( Math_utils::EqualParity(nums1) ==true);
+  REQUIRE( Math_utils::EqualParity(nums2) == true);
+  REQUIRE( Math_utils::EqualParity(nums3)==false);
+  
+}
